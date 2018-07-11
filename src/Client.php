603d2b3b87d06d6e6b343e018aa088a1e382a3b6
Hedcler Morais <hedclermorais@gmail.com>
2018-07-11 15:23:35 -0300

    Update Client.php
    Change secret key

--- src/Client.php
+++ src/Client.php
@@ -6,7 +6,7 @@ use Sinesp\Exceptions\SinespException;
 
 class Client
 {
-	const SECRET = '#8.1.0#Mw6HqdLgQsX41xAGZgsF';
+	const SECRET = '#8.1.0#g8LzUadkEHs7mbRqbX5l';
     const URL = 'https://cidadao.sinesp.gov.br/sinesp-cidadao/mobile/consultar-placa/v3';
     
     private $proxy = array();
